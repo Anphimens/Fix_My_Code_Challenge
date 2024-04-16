dc4a4714b8510ecfe7c81eb65bfd692c4ac1ad1f
Anphimens <philipinamensah19@gmail.com>
2024-04-16 23:38:35 +0100

    Pycodestyle requirements fixed

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 class Square:
     def __init__(self, width=0, height=0):
         self.width = width
@@ -18,8 +19,8 @@ class Square:
         square dimension """
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
